b17c104e7b8c7ae9d186e9756d5fb8eeec48baaa
xmhk <chmhnk@googlemail.com>
2013-11-01 16:37:20 +0100

    added functionality to work with local processors

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # gridiot.py
 
-Rev 4, oct 23, 2013
+Rev 5, nov 1, 2013
 
 * you will need automatic login via ssh-keys
 * on remote machines, your will need some RC file that sets your path for non-interactive shells
+* be careful with the clear\_node\_workdirs and pull\_from\_nodes functions!
 * * *
 
 ##class Computing\_Node()
@@ -12,8 +13,9 @@ Rev 4, oct 23, 2013
     * ip
     * port
     * workdir
-    * freeslots
-    * mode
+    * freeslots     -> number of cores to use
+    * mode          -> 'remote' or 'local'
+    * returndir     -> local dir that will be synced to after joblist is done
 
 ###\_\_init\_\_( username, ip, port, workdir, maxslots, mode)
 
@@ -36,7 +38,7 @@ Rev 4, oct 23, 2013
 ##process\_list(joblist,nodelist)
 
 ##push\_to\_nodes( nodelist, pushfilelist)
-* a list of files (or wildcards) in pushfilelist are transferred to the workdir on the remote machine
+* a list of files (or wildcards) in pushfilelist are transferred to the workdir on the remote machine / local dir
 
 ##pull\_from\_nodes(nodelist)
 * be careful with this!!! remote files will overwrite files that were pushed and that changed in the meantime!!!
@@ -46,3 +48,6 @@ Rev 4, oct 23, 2013
 
 ##print_df(nodelist):
 * exectute df on all nodes to get the free disk space
+
+##addslash(directorystring)
+* will add a slash '/' to a string. Is used when building rsync commands to prevent file/directory hazzle
--- gridiot.py
+++ gridiot.py
@@ -1,42 +1,60 @@
 from subprocess import Popen
 from time import sleep
-
-
 from os import system as ossystem
 from sys import argv as sysargv
 
+def addslash( directory ):  #make sure that directories end with '/' for rsync commands
+    if directory[len(directory)-1] != "/":
+        directory+="/"
+        #print "added"
+    return directory
 
 
-class Computing_Node():
+class Computing_Node(): #represents a local or remote computing node
     def __init__(self,
                  username,
                  ip,
                  port,
                  workdir,
                  maxslots,
-                 mode):
+                 mode,
+                 returndir):
         self.username = username
         self.ip = ip
         self.port = port
         self.workdir = workdir
         self.freeslots = maxslots
         self.mode = mode
+        self.returndir = returndir
 
     def get_command(self, ccommand):
-        sshpart = "ssh %s@%s -p %d"%(self.username, self.ip,self.port)
-        return "%s 'cd %s && %s'"%(sshpart,self.workdir,ccommand)
-    
+        if self.mode.lower() == 'remote':
+            sshpart = "ssh %s@%s -p %d"%(self.username, self.ip,self.port)
+            return "%s 'cd %s && %s'"%(sshpart,self.workdir,ccommand)
+        else: #local
+            return "cd %s && %s"%(self.workdir,ccommand)
+
     def clear_workdir(self):
-        print "--- clearing working directory on %s@%s"%(self.username, self.ip)
-        ccommand  = "ssh %s@%s -p %d 'cd && rm -fr %s'"%(self.username, self.ip,self.port, self.workdir)
-        ossystem( ccommand )
-        
+        if self.mode.lower() == 'remote':
+            print "--- clearing working directory on %s@%s"%(self.username, self.ip)
+            ccommand  = "ssh %s@%s -p %d 'cd && rm -fr %s'"%(self.username, self.ip,self.port, self.workdir)
+            ossystem( ccommand )
+        else: 
+            print "--- clearing local working directory on %s@%s"%(self.username, self.ip)
+            ccommand = "rm -fr %s"%self.workdir
+            ossystem(ccommand)
+          #  print ccommand
+         #   print "\n\n\n !!! TODO write a secure local clearing!!!\n\n\n "
+
     def print_df(self, extracommand):
-        ccommand  = "ssh %s@%s -p %d 'df -h -l %s'"%(self.username, self.ip,self.port,extracommand)
-        ossystem( ccommand )
+        if self.mode.lower() == 'remote':
+            ccommand  = "ssh %s@%s -p %d 'df -h -l %s'"%(self.username, self.ip,self.port,extracommand)
+            ossystem( ccommand )
+        else:
+            ossystem( "df -h")
+        
 
-
-class Job_Object():
+class Job_Object():  #represents a job
     def __init__(self, ccommand):
         self.ccommand = ccommand
         self.done = False
@@ -52,7 +70,7 @@ class Job_Object():
         self.active = True
         self.process = Popen( self.node.get_command(self.ccommand),shell=True,stdout=None)
         self.node.freeslots-=1
-#        print "starting job on %s@%s"%(self.node.username,self.node.ip)
+
 
     def check( self):
         if self.done==False and self.started == True:
@@ -103,24 +121,37 @@ def process_list(joblist,nodelist):
 
 def push_to_nodes( nodelist, pushfilelist):
     for node in nodelist:
-        ccommand = 'rsync -rlzvhc --progress --exclude=%s '%(sysargv[0]) #you don't want to push your control file
-        # compression on switch = -z
+        if node.mode.lower() == 'remote':
+            ccommand = 'rsync -rlzvhc --progress --exclude=%s '%(sysargv[0]) #you don't want to push your control file
+# compression on switch = -z
+            for pd in pushfilelist:
+                ccommand+=pd+' '
+            ccommand += ' -e "ssh -p %s"  %s@%s:%s'%(node.port,node.username,node.ip,addslash( node.workdir))
+            print "++ rsync push command = ",ccommand
+            ossystem(ccommand)
+        else:  #local
+            ccommand = 'rsync -rlzvhc --progress --exclude=%s '%(sysargv[0])
+            for pd in pushfilelist:
+                ccommand+=pd+' '
+            ccommand += ' %s'%(addslash( node.workdir))
+            print "++ local rsync push command = ",ccommand
+            ossystem(ccommand)
 
-        for pd in pushfilelist:
-            ccommand+=pd+' '
-        ccommand += ' -e "ssh -p %s"  %s@%s:%s'%(node.port,node.username,node.ip,node.workdir)
-        print "++ rsynccommand = ",ccommand
-        ossystem(ccommand)
+            
 
 
 
 def pull_from_nodes(nodelist):
     for node in nodelist:
-        ccommand = 'rsync -rlvzhc --progress %s@%s:%s ./ -e "ssh -p %s" --exclude=%s  '%(node.username,node.ip,node.workdir,node.port,sysargv[0])
-        # compression on switch = -z
-
-        print "++ rsynccommand = ",ccommand
-        ossystem(ccommand)
+        if node.mode.lower() == 'remote':
+            ccommand = 'rsync -rlvzhc --progress %s@%s:%s/* %s -e "ssh -p %s" --exclude=%s  '%(node.username,node.ip,node.workdir,addslash( node.returndir),node.port,sysargv[0])
+# compression on switch = -z
+            print "++ rsync pull command = ",ccommand
+            ossystem(ccommand)
+        else: #local node
+            ccommand = 'rsync -rlvzhc --progress %s/* %s  '%(node.workdir,addslash( node.returndir))
+            print "++ local rsync pull command : ",ccommand
+            ossystem(ccommand)
 
 
 def clear_node_workdirs(nodelist):
